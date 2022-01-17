2c9adb70f3b8e57b479952f442cbda2b4d7fc90b
rodmartinez <izeka1983@hotmail.com>
2022-01-16 21:41:53 -0300

    test print stdout

---
+++ expensereport-go/expenses/expensereport_test.go
@@ -0,0 +1,33 @@
+package expenses
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"testing"
+)
+
+func captureStdout(f func()) string {
+	old := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	f()
+
+	w.Close()
+	os.Stdout = old
+
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+	return buf.String()
+}
+
+func TestPrintEmptyReport(t *testing.T) {
+	expenses := []Expense{{}}
+	printReport(expenses)
+}
+
+func TestPrintReport(t *testing.T) {
+	expenses := []Expense{{1, 10}, {2, 20}}
+	printReport(expenses)
+}
