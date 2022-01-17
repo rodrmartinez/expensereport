506dbc150f7a84025168857ed08db7281684426d
rodmartinez <izeka1983@hotmail.com>
2022-01-16 22:51:30 -0300

    first refactor test

--- expensereport-go/expenses/expensereport_test.go
+++ expensereport-go/expenses/expensereport_test.go
@@ -1,26 +1,10 @@
 package expenses
 
 import (
-	"bytes"
-	"io"
-	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
-
-func captureStdout(f func()) string {
-	old := os.Stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-
-	f()
-
-	w.Close()
-	os.Stdout = old
-
-	var buf bytes.Buffer
-	io.Copy(&buf, r)
-	return buf.String()
-}
 
 func TestPrintEmptyReport(t *testing.T) {
 	expenses := []Expense{{}}
@@ -40,4 +24,47 @@ func TestPrintReportLunch(t *testing.T) {
 func TestPrintReportOverExpenses(t *testing.T) {
 	expenses := []Expense{{4, 3000}}
 	printReport(expenses)
+}
+
+func TestExpenseValid(t *testing.T) {
+	expense := Expense{1, 5000}
+	got := expenseValid(expense)
+	assert.Equal(t, " ", got)
+
+	expense = Expense{1, 5001}
+	got = expenseValid(expense)
+	assert.Equal(t, "X", got)
+
+	expense = Expense{2, 1001}
+	got = expenseValid(expense)
+	assert.Equal(t, "X", got)
+
+}
+
+func TestGetExpenseName(t *testing.T) {
+	expense := Expense{1, 10}
+	got := getExpenseName(expense)
+	assert.Equal(t, "Dinner", got)
+
+	expense = Expense{2, 10}
+	got = getExpenseName(expense)
+	assert.Equal(t, "Breakfast", got)
+
+	expense = Expense{3, 10}
+	got = getExpenseName(expense)
+	assert.Equal(t, "Car Rental", got)
+
+}
+
+func TestGetExpenses(t *testing.T) {
+	expenses := []Expense{{1, 10}}
+	meals, total := getExpenses(expenses)
+	assert.Equal(t, 10, meals)
+	assert.Equal(t, 10, total)
+
+    expenses = []Expense{{1, 10},{2,50},{3,150}}
+	meals, total = getExpenses(expenses)
+	assert.Equal(t, 60, meals)
+	assert.Equal(t, 210, total)
+
 }
