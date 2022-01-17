0084accc3956b46d0abb01ad39186c45d4aeed2e
rodmartinez <izeka1983@hotmail.com>
2022-01-16 22:51:02 -0300

    refactor code

--- expensereport-go/expenses/expensereport.go
+++ expensereport-go/expenses/expensereport.go
@@ -19,40 +19,48 @@ type Expense struct {
 	Amount int
 }
 
-func printReport(expenses []Expense) {
+func getExpenseName(expense Expense) string {
+	switch expense.Type {
+	case DINNER:
+		return "Dinner"
+	case BREAKFAST:
+		return "Breakfast"
+	case CAR_RENTAL:
+		return "Car Rental"
+	default:
+		return "Expense not found"
+	}
+}
+
+func expenseValid(expense Expense) string {
+	if expense.Type == DINNER && expense.Amount > 5000 || expense.Type == BREAKFAST && expense.Amount > 1000 {
+		return "X"
+	} else {
+		return " "
+	}
+}
+
+func getExpenses(expenses []Expense) (int, int) {
 	total := 0
 	mealExpenses := 0
 
-	fmt.Printf("Expenses %s\n", time.Now().Format("2006-01-02"))
-
 	for _, expense := range expenses {
-		if expense.Type == DINNER || expense.Type == BREAKFAST || expense.Type == LUNCH {
+		if expense.Type != CAR_RENTAL {
 			mealExpenses += expense.Amount
 		}
 
-		var expenseName string
-		switch expense.Type {
-		case DINNER:
-			expenseName = "Dinner"
-		case BREAKFAST:
-			expenseName = "Breakfast"
-		case CAR_RENTAL:
-			expenseName = "Car Rental"
-		case LUNCH:
-			expenseName = "Lunch"
-		}
-
-		var mealOverExpensesMarker string
-		if expense.Type == DINNER && expense.Amount > 5000 || expense.Type == BREAKFAST && expense.Amount > 1000 || expense.Type == LUNCH && expense.Amount > 2000 {
-			mealOverExpensesMarker = "X"
-		} else {
-			mealOverExpensesMarker = " "
-		}
-
-		fmt.Printf("%s\t%d\t%s\n", expenseName, expense.Amount, mealOverExpensesMarker)
 		total += expense.Amount
 	}
+	return mealExpenses, total
+}
 
+func printReport(expenses []Expense) {
+	fmt.Printf("Expenses %s\n", time.Now().Format("2006-01-02"))
+	for _, expense := range expenses {
+		fmt.Printf("%s\t%d\t%s\n", getExpenseName(expense), expense.Amount, expenseValid(expense))
+	}
+	mealExpenses, total := getExpenses(expenses)
 	fmt.Printf("Meal expenses: %d\n", mealExpenses)
 	fmt.Printf("Total expenses: %d\n", total)
+
 }
