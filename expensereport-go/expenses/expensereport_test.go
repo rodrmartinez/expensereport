68c8bdc9785fdcdf47270e07a38fab71e666128c
rodmartinez <izeka1983@hotmail.com>
2022-01-16 23:23:49 -0300

    Add assertion tests

--- expensereport-go/expenses/expensereport_test.go
+++ expensereport-go/expenses/expensereport_test.go
@@ -4,26 +4,30 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	approvaltests "github.com/approvals/go-approval-tests"
 )
 
 func TestPrintEmptyReport(t *testing.T) {
 	expenses := []Expense{{}}
-	printReport(expenses)
+	approvaltests.VerifyString(t, printReport(expenses))
 }
 
 func TestPrintReport(t *testing.T) {
-	expenses := []Expense{{1, 10}, {2, 20}}
-	printReport(expenses)
+	expenses := []Expense{{1, 10}, {2, 20},{3,40}}
+	approvaltests.VerifyString(t, printReport(expenses))
+
 }
 
 func TestPrintReportLunch(t *testing.T) {
 	expenses := []Expense{{4, 30}}
-	printReport(expenses)
+	approvaltests.VerifyString(t, printReport(expenses))
 }
 
 func TestPrintReportOverExpenses(t *testing.T) {
 	expenses := []Expense{{4, 3000}}
-	printReport(expenses)
+	approvaltests.VerifyString(t, printReport(expenses))
+
 }
 
 func TestExpenseValid(t *testing.T) {
@@ -62,7 +66,7 @@ func TestGetExpenses(t *testing.T) {
 	assert.Equal(t, 10, meals)
 	assert.Equal(t, 10, total)
 
-    expenses = []Expense{{1, 10},{2,50},{3,150}}
+	expenses = []Expense{{1, 10}, {2, 50}, {3, 150}}
 	meals, total = getExpenses(expenses)
 	assert.Equal(t, 60, meals)
 	assert.Equal(t, 210, total)
@@ -75,7 +79,7 @@ func TestGetLunchName(t *testing.T) {
 	assert.Equal(t, "Lunch", got)
 }
 
-func TestaLunchValid(t *testing.T) {
+func TestLunchValid(t *testing.T) {
 	expense := Expense{4, 50}
 	got := expenseValid(expense)
 	assert.Equal(t, " ", got)
@@ -92,7 +96,7 @@ func TestGetExpenseWithLunch(t *testing.T) {
 	assert.Equal(t, 10, meals)
 	assert.Equal(t, 10, total)
 
-    expenses = []Expense{{1, 10},{2,50},{3,150},{4,30}}
+	expenses = []Expense{{1, 10}, {2, 50}, {3, 150}, {4, 30}}
 	meals, total = getExpenses(expenses)
 	assert.Equal(t, 90, meals)
 	assert.Equal(t, 240, total)
